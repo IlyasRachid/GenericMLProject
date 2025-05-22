2035bb300840c0e9b152e32577cab6dc09d6eff2
Ilvan <ilyasrachid00@gmail.com>
2025-05-22 14:43:03 +0100

    Model training added

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -15,3 +16,17 @@ def save_object(file_path, obj):
                 dill.dump(obj, file_obj)
     except Exception as e:
          raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, model):
+    try:
+        report = {}
+        for i in range(len(list(model))):
+            model_name = list(model.values())[i]
+            model_name.fit(X_train, y_train)
+            y_test_pred = model_name.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(model.keys())[i]] = test_model_score
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)    
