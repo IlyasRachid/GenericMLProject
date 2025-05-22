2e1d3f6fb0f26b5547a7c4f880785fe713cae2e1
Ilvan <ilyasrachid00@gmail.com>
2025-05-22 16:44:40 +0100

    hyperparameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,7 +45,54 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor()
             }
 
-            model_report: dict= evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test, y_test=y_test, model=models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    #'splitter': ['best', 'random'],
+                    #'max_depth': [None, 10, 20, 30, 40, 50],
+                    #'max_features': ['auto', 'sqrt', 'log2'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8,16,32,64,128,256],
+                    #'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Gradient Boosting": {
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate': [.1, .01, .05, .001],
+                    "subsample": [0.6, 0.7, 0.8, 0.85, 0.9],
+                    #'max_depth': [3, 5, 7],
+                    #'min_samples_split': [2, 5, 10],
+                    #'min_samples_leaf': [1, 2, 4],
+                },
+                "Linear Regression": {},
+                "K-Neighbors Classifier": {
+                    'n_neighbors': [5, 7, 9, 11],
+                    #'weights': ['uniform', 'distance'],
+                    #'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    #'leaf_size': [10, 20, 30],
+                },
+                "XGBClassifier": {
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate': [.1, .01, .05, .001],
+                    #'max_depth': [3, 5, 7],
+                    #'min_child_weight': [1, 2, 3],
+                    #'subsample': [0.6, 0.7, 0.8, 0.85, 0.9],
+                },
+                "CatBoosting Classifier": {
+                    'iterations': [30,50,100],
+                    'learning_rate': [.1, .01, .05, .001],
+                    'depth': [6,8,10],
+                    #'l2_leaf_reg': [1, 3, 5],
+                },
+                "AdaBoost Classifier": {
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate': [.1, .01, .05, .001],
+                    #'base_estimator': [None, DecisionTreeRegressor(max_depth=3)],
+                }
+
+            }
+
+            model_report: dict= evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test, y_test=y_test, model=models, param=params)
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +18,17 @@ def save_object(file_path, obj):
     except Exception as e:
          raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, model):
+def evaluate_model(X_train, y_train, X_test, y_test, model, param):
     try:
         report = {}
         for i in range(len(list(model))):
             model_name = list(model.values())[i]
+            para = param[list(model.keys())[i]]
+            gs = GridSearchCV(model_name, para, cv=3)
+            gs.fit(X_train, y_train)
+            model_name.set_params(**gs.best_params_)
             model_name.fit(X_train, y_train)
+            #model_name.fit(X_train, y_train)
             y_test_pred = model_name.predict(X_test)
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(model.keys())[i]] = test_model_score
